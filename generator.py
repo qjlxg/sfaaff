b9f8c01fde8b623d838bf1306fb8c28268963d2c
qjlxg <qjlxg@163.com>
2026-09-19 01:01:33 +0800

    更新 generator.py

--- generator.py
+++ generator.py
@@ -22,7 +22,7 @@ IP_SOURCES = {
 }
 
 NODES_DIR = Path("nodes")
-TEMPLATE_FILE: Optional[Path] = Path("nodes/vmess/002.txt") # 例如 Path("nodes/vmess/001.txt")，None 表示自动扫描  nodes/trojan/001.txt    nodes/vmess/001.txt  nodes/vless/001.txt nodes/vless/002.txt
+TEMPLATE_FILE: Optional[Path] = Path("nodes/vmess/003.txt") # 例如 Path("nodes/vmess/001.txt")，None 表示自动扫描  nodes/trojan/001.txt    nodes/vmess/001.txt  nodes/vless/001.txt nodes/vless/002.txt
 
 OUTPUT_DIR = Path("generated")
 PROBE_IP_COUNT = 30
